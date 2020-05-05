c40e6407ff4d037bf4c1f39021996954ed4a33ee
Nick Taylor <nickstaylor@gmail.com>
2020-05-05 16:17:37 -0600

    iterations 0-2 for React functionality

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = (props) => {
+  return (
+    <div className="news-article">
+    <img src={props.img} alt={props.headline} />
+    <section className="article-body">
+    <h3>{props.headline}</h3>
+    <p>{props.description}</p>
+    </section>
+    <a href={props.url} target='_blank'><button className="article-button">Link to Article ➡️</button></a>
+    </div>
+  )
 
+
+}
 export default NewsArticle;
