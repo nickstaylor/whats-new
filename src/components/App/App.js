4833691b0be06b8bb5225212357c67e02100700f
Nick Taylor <nickstaylor@gmail.com>
2020-05-05 16:17:45 -0600

    iterations 0-2 for React functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
+
+
 import './App.css';
+import Menu from '../Menu/Menu'
+import NewsContainer from '../NewsContainer/NewsContainer'
+import SearchForm from '../SearchForm/SearchForm'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local: local,
+      entertainment: entertainment,
+      health: health,
+      science: science,
+      technology: technology,
+      chosen: local
     }
   }
+
+  chooseCategory = (event) =>{
+  const { name } = event.target
+   this.setState({chosen: this.state[name]})
+ }
+
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <SearchForm />
+        <Menu chooseCategory={this.chooseCategory}/>
+        <NewsContainer data={this.state.chosen} />
       </div>
-    );
+    )
   }
 }
 
