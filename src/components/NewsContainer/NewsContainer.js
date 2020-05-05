2d91adeeaf778b19c171c94291c9be4e134733da
Nick Taylor <nickstaylor@gmail.com>
2020-05-05 16:17:32 -0600

    iterations 0-2 for React functionality

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = (props) => {
+  console.log('newscontainerprops', props)
+  let category = props.data
+  const newsArticles = category.map(item=> {
+    return (
+      <NewsArticle
+      key = {item.id}
+      headline = {item.headline}
+      img = {item.img}
+      description = {item.description}
+      url = {item.url}
+      id = {item.id}
+      />
+    )
+  })
+
+  return (
+    <section className="news-container">
+    {newsArticles}
+    </section>
+  )
+}
 
 export default NewsContainer;
