c718b078aee391806ce17c41d69f40f59f261939
Nick Taylor <nickstaylor@gmail.com>
2020-05-05 20:51:52 -0600

    add search functionality-iteration 3

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -4,8 +4,41 @@ import NewsArticle from '../NewsArticle/NewsArticle'
 
 const NewsContainer = (props) => {
   console.log('newscontainerprops', props)
-  let category = props.data
-  const newsArticles = category.map(item=> {
+  const { filteredData, searchInput, data} = props
+  let filteredArticles
+  let newsArticles
+
+  if (filteredData){
+      // props.resetFilter()
+      filteredArticles = data.filter(item=>{
+      return item.headline.toLowerCase().includes(searchInput.toLowerCase())
+    })
+    if (filteredArticles.length === 0){
+        newsArticles =
+        <div>
+        So sorry. The search results are empty. Please click on a menu category to view newsArticles
+        </div>
+
+    } else {
+        newsArticles = filteredArticles.map(item=> {
+        return (
+          <NewsArticle
+          key = {item.id}
+          headline = {item.headline}
+          img = {item.img}
+          description = {item.description}
+          url = {item.url}
+          id = {item.id}
+          />
+        )
+      })
+    }
+    // props.resetFilter()
+    console.log('filteredArticles', filteredArticles)
+  }
+
+  if (!filteredArticles){
+    newsArticles = data.map(item=> {
     return (
       <NewsArticle
       key = {item.id}
@@ -17,6 +50,7 @@ const NewsContainer = (props) => {
       />
     )
   })
+}
 
   return (
     <section className="news-container">
