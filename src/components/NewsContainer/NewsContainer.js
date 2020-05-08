8f4bd05b7ae0ef1cec63d4bfc80e5062d2b26e3a
Nick Taylor <nickstaylor@gmail.com>
2020-05-07 18:07:25 -0600

    refactor, clear console logs

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,62 +1,64 @@
 import React from 'react';
 import './NewsContainer.css'
 import NewsArticle from '../NewsArticle/NewsArticle'
+import PropTypes from 'prop-types'
 
 const NewsContainer = (props) => {
-  console.log('newscontainerprops', props)
   const { filteredData, searchInput, data} = props
   let filteredArticles
   let newsArticles
 
   if (filteredData){
-      // props.resetFilter()
       filteredArticles = data.filter(item=>{
       return item.headline.toLowerCase().includes(searchInput.toLowerCase())
-    })
-    if (filteredArticles.length === 0){
-        newsArticles =
-        <div>
-        So sorry. The search results are empty. Please click on a menu category to view newsArticles
-        </div>
-
+      })
+  if (filteredArticles.length === 0){
+      newsArticles =
+      <div>
+        <p className="empty-search">So sorry. The search results are empty. Please click on a menu category to view News Articles</p>
+      </div>
     } else {
         newsArticles = filteredArticles.map(item=> {
-        return (
-          <NewsArticle
+          return (
+            <NewsArticle
+              key = {item.id}
+              headline = {item.headline}
+              img = {item.img}
+              description = {item.description}
+              url = {item.url}
+              id = {item.id}
+            />
+          )
+        })
+    }
+  }
+
+  if (!filteredArticles){
+    newsArticles = data.map(item=> {
+      return (
+        <NewsArticle
           key = {item.id}
           headline = {item.headline}
           img = {item.img}
           description = {item.description}
           url = {item.url}
           id = {item.id}
-          />
-        )
-      })
-    }
-    // props.resetFilter()
-    console.log('filteredArticles', filteredArticles)
+        />
+      )
+    })
   }
-
-  if (!filteredArticles){
-    newsArticles = data.map(item=> {
-    return (
-      <NewsArticle
-      key = {item.id}
-      headline = {item.headline}
-      img = {item.img}
-      description = {item.description}
-      url = {item.url}
-      id = {item.id}
-      />
-    )
-  })
-}
 
   return (
     <section className="news-container">
-    {newsArticles}
+      {newsArticles}
     </section>
   )
+}
+
+NewsContainer.propTypes = {
+  filteredData: PropTypes.bool,
+  searchInput: PropTypes.string,
+  data: PropTypes.array
 }
 
 export default NewsContainer;
