08a8e9500bb469ef0ee89c4f694d8fba7a8a647f
Nick Taylor <nickstaylor@gmail.com>
2020-05-07 19:40:29 -0600

    remove debugs on app.test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -7,20 +7,20 @@ import App from './App';
 describe('App', ()=>{
   it('changes news topics when a menu topic is clicked', ()=>{
 
-    const { debug, getByAltText, getByText } = render(<App />)
+    const { getByAltText, getByText } = render(<App />)
     fireEvent.click(getByText('Science'))
-    // debug()
+
       expect(getByAltText('NASA, in Reddit AMA, says Earth probably won\'t be eaten by a black hole')).toBeInTheDocument()
       expect(getByAltText(/moon/)).toBeInTheDocument()
 
   })
 
   it('should show filtered articles based on search input',() => {
-      const { debug, getByPlaceholderText, getByText, getByAltText, findAllByText } = render(<App />)
+      const { getByPlaceholderText, getByText, getByAltText, findAllByText } = render(<App />)
       fireEvent.click(getByText('Science'))
       fireEvent.change(getByPlaceholderText('search for articles here'), {target: {value: "Lockheed"}})
       fireEvent.click(getByText('Search'))
-      debug()
+
       expect(getByAltText(/Lockheed/)).toBeInTheDocument();
   })
 
