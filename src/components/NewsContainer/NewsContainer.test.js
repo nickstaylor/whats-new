9524eacfd6cfedb442c8c8db2f8cdefa7ac4ebf7
Nick Taylor <nickstaylor@gmail.com>
2020-05-07 19:40:50 -0600

    add newsContainer tests

---
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import NewsContainer from './NewsContainer';
+
+
+describe('NewsContainer', ()=>{
+
+  it('should display NewsArticles', ()=>{
+    const { getByText } = render(<NewsContainer
+                                  data={[{id: 1,
+                                     description: "awesomeness",
+                                     headline: "you are not cool",
+                                     url: "http://uncool.com",
+                                     img: "notCoolAtAll"}]} />)
+
+    expect(getByText('you are not cool')).toBeInTheDocument()
+  })
+
+  it('should show message if no articles are in the search', ()=>{
+    const { getByText } = render(<NewsContainer
+                                  data={[{id: 1,
+                                     description: "awesomeness",
+                                     headline: "you are not cool",
+                                     url: "http://uncool.com",
+                                     img: "notCoolAtAll"}]}
+                                     searchInput="nothing"
+                                     filteredData={true} />)
+
+    expect(getByText('So sorry. The search results are empty. Please click on a menu category to view News Articles')).toBeInTheDocument()
+  })
+
+
+})
