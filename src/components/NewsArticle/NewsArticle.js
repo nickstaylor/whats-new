cd3e6bf3b5bd4b5226a3cad321fbb2534cc6aa8e
Nick Taylor <nickstaylor@gmail.com>
2020-05-07 18:06:02 -0600

    refactor of NewsArticle.js

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './NewsArticle.css';
+import PropTypes from 'prop-types';
+
+
 
 const NewsArticle = (props) => {
   return (
     <div className="news-article">
-    <img src={props.img} alt={props.headline} />
-    <section className="article-body">
-    <h3>{props.headline}</h3>
-    <p>{props.description}</p>
-    </section>
-    <a href={props.url} target='_blank'><button className="article-button">Link to Article ➡️</button></a>
+      <img className="article-image" src={props.img} alt={props.headline} />
+      <div className="article-body">
+        <h3>{props.headline}</h3>
+        <p>{props.description}</p>
+      </div>
+      <a href={props.url} target='_blank' rel="noopener noreferrer"><button className="article-button">Link to Article ➡</button></a>
     </div>
   )
-
-
 }
+
+
+NewsArticle.propTypes = {
+  img: PropTypes.string,
+  headline: PropTypes.string,
+  description: PropTypes.string,
+  id: PropTypes.number,
+}
+
+
 export default NewsArticle;
