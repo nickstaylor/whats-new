e24221a53b652592f8c8a6149319800b7995e2e7
Nick Taylor <nickstaylor@gmail.com>
2020-05-07 18:06:58 -0600

    change section to form on searchform

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './SearchForm.css';
+import PropTypes from 'prop-types'
 
 const SearchForm = (props) =>{
   return (
     <div className='header'>
-    <h2>One Sweet News App!</h2>
-    <section className='search-bar'>
-      <input
-      type="text"
-      placeholder="search for articles here"
-      value={props.searchInput}
-      onChange={props.handleChange}
-      />
-      <button onClick={props.submitSearch}>Search</button>
-    </section>
+      <h2>One Sweet News App!</h2>
+      <form className='search-bar'>
+        <input
+          type="text"
+          placeholder="search for articles here"
+          value={props.searchInput}
+          onChange={props.handleChange}
+        />
+        <button onClick={props.submitSearch}>Search</button>
+      </form>
     </div>
   )
+}
+
+SearchForm.propTypes = {
+  handleChange: PropTypes.func,
+  searchInput: PropTypes.string,
+  submitSearch: PropTypes.func
 }
 
 export default SearchForm;
