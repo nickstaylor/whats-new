077b6373b005b15632d36aa29353cc507d847761
Nick Taylor <nickstaylor@gmail.com>
2020-06-23 15:57:32 -0600

    Switching to Travis org

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 import NewsContainer from '../NewsContainer/NewsContainer'
 
 describe('App', ()=>{
-  it.skip('changes news topics when a menu topic is clicked', () => {
+  it('changes news topics when a menu topic is clicked', () => {
 
     const { getByAltText, getByText } = render(<App />)
     fireEvent.click(getByText('Science'))
@@ -16,30 +16,30 @@ describe('App', ()=>{
 
   })
 
-  it.skip('should show filtered articles based on search input', async () => {
+  it('should show filtered articles based on search input', () => {
       const { getByPlaceholderText, getByText, getByAltText, findAllByText } = render(<App />)
       fireEvent.click(getByText('Science'))
       fireEvent.change(getByPlaceholderText('search for articles here'), {target: {value: "Lockheed"}})
       fireEvent.click(getByText('Search'))
       // had to do only one return because multiple returns created an array and I didn't know
       //how to test that I tried to use findAllByText
-      const result = await waitFor(() => getByAltText(/Lockheed/))
+      const result = getByAltText(/Lockheed/)
       expect(result).toBeInTheDocument()
   })
 
-  it.skip('should show message if no articles are in the search', ()=>{
-    const { getByText, getByPlaceholderText } = render(<NewsContainer
-                                  data={[{id: 1,
-                                     description: "awesomeness",
-                                     headline: "you are not cool",
-                                     url: "http://uncool.com",
-                                     img: "notCoolAtAll"}]}
-                                    //  searchInput="nothing"
-                                     // filteredData={true}
-                                      />)
-    fireEvent.change(getByPlaceholderText('search for articles here'), {target: {value: "nothing"}})
-    expect(getByText('So sorry. The search results are empty. Please click on a menu category to view News Articles')).toBeInTheDocument()
-  })
+  // it('should show message if no articles are in the search', ()=>{
+  //   const { getByText, getByPlaceholderText } = render(<NewsContainer
+  //                                 data={[{id: 1,
+  //                                    description: "awesomeness",
+  //                                    headline: "you are not cool",
+  //                                    url: "http://uncool.com",
+  //                                    img: "notCoolAtAll"}]}
+  //                                    // searchInput="nothing"
+  //                                    // filteredData={true}
+  //                                     />)
+  //   fireEvent.change(getByPlaceholderText('search for articles here'), {target: {value: "nothing"}})
+  //   expect(getByText('So sorry. The search results are empty. Please click on a menu category to view News Articles')).toBeInTheDocument()
+  // })
 
 
 })
--- src/components/NewsContainer/NewsContainer.test.js
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -17,19 +17,7 @@ describe('NewsContainer', ()=>{
     expect(getByText('you are not cool')).toBeInTheDocument()
   })
 
-  // it('should show message if no articles are in the search', ()=>{
-  //   const { getByText, getByPlaceholderText } = render(<NewsContainer
-  //                                 data={[{id: 1,
-  //                                    description: "awesomeness",
-  //                                    headline: "you are not cool",
-  //                                    url: "http://uncool.com",
-  //                                    img: "notCoolAtAll"}]}
-  //                                   //  searchInput="nothing"
-  //                                    // filteredData={true}
-  //                                     />)
-  //   fireEvent.change(getByPlaceholderText('search for articles here'), {target: {value: "nothing"}})
-  //   expect(getByText('So sorry. The search results are empty. Please click on a menu category to view News Articles')).toBeInTheDocument()
-  // })
+
 
 
 })
